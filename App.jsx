7605a1fa735ef0860df9adea50c6c894239f9f4b
Raj Gupta <154043831+Rajgupta9546@users.noreply.github.com>
2026-03-03 18:09:54 +0530

    Improve UI and code cleanup
    Improve UI and code cleanup

--- App.jsx
+++ App.jsx
@@ -1,15 +1,27 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import BrowseBooks from "./pages/BrowseBooks";
+import BookDetails from "./pages/BookDetails";
 import AddBook from "./pages/AddBook";
+import NotFound from "./pages/NotFound";
 
 function App() {
+  const location = useLocation();
+
   return (
-    <BrowserRouter>
+    <>
+      {location.pathname !== "/404" && !location.pathname.includes("invalid") && <Navbar />}
+
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/books" element={<BrowseBooks />} />
+        <Route path="/books/:category" element={<BrowseBooks />} />
+        <Route path="/book/:id" element={<BookDetails />} />
         <Route path="/add" element={<AddBook />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
