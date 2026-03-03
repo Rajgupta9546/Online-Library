1615c06f6af2ec2deddbc5be17be711a8e00db25
Raj Gupta <154043831+Rajgupta9546@users.noreply.github.com>
2026-03-03 17:46:08 +0530

    Add dummy book data and initialize Redux state
    Add dummy book data and initialize Redux state

---
+++ dummyBooks.js
@@ -0,0 +1,38 @@
+export const initialBooks = [
+  {
+    id: "1",
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    category: "Fiction",
+    description: "A fantasy adventure novel about Bilbo Baggins.",
+    rating: 4.8,
+    image: "https://covers.openlibrary.org/b/id/6979861-L.jpg"
+  },
+  {
+    id: "2",
+    title: "Harry Potter",
+    author: "J.K. Rowling",
+    category: "Fiction",
+    description: "A young wizard’s magical journey.",
+    rating: 4.9,
+    image: "https://covers.openlibrary.org/b/id/7984916-L.jpg"
+  },
+  {
+    id: "3",
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    category: "Fiction",
+    description: "A philosophical novel about dreams.",
+    rating: 4.6,
+    image: "https://covers.openlibrary.org/b/id/8231856-L.jpg"
+  },
+  {
+    id: "4",
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    category: "Fiction",
+    description: "A story of wealth and tragedy.",
+    rating: 4.4,
+    image: "https://covers.openlibrary.org/b/id/7222246-L.jpg"
+  },
+];
