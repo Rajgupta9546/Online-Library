922fd333e69cd0ad093a9782264e0d329de344a2
Raj Gupta <154043831+Rajgupta9546@users.noreply.github.com>
2026-03-03 16:58:12 +0530

    Initial project setup using Vite and React
    Initial project setup using Vite and React

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import AddBook from "./pages/AddBook";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddBook />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
